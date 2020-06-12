1e2f76f5c674e0c3738c06b1d9a32055c1c89b16
Sanjari Alfian <sanjari.alfian.01@gmail.com>
2020-06-12 19:40:53 +0700

    First Commit

--- routes/index.js
+++ routes/index.js
@@ -2,11 +2,9 @@ const express = require('express');
 
 const router = express.Router();
 
-/* GET home page. */
 router.get('/', function (req, res, next) {
   res.send({
-    nama: "sanjari",
-    NIM: "11180910000083"
+    message: "Selamat Datang Di Program SANS-API"
   })
 });
 
